3bff0200b5f347cdfd1ad571167649db0b2ae14e
FairyPenguin <autorun81@gmail.com>
2023-12-13 01:58:59 +0200

    root-level logger middleware

--- myApp.js
+++ myApp.js
@@ -8,31 +8,29 @@ const absolutePathHTMl = __dirname + "/views/index.html";
 const absolutePathCSS = __dirname + "/public";
 
 // root-level middleware logger
-app.use(function middleware(req, res, next)  {
+app.use(function middleware(req, res, next) {
   const responseMessage = `${req.method} ${req.path} - ${req.ip}`;
-  res.send(responseMessage);
+  console.log(responseMessage);
   next();
 });
-
-GET /json - 156.192.167.40
 
 app.use("/public", express.static(__dirname + "/public"));
 
 // send file response example
-app.get("/", (req, res) => {
-  res.sendFile(absolutePathHTMl);
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(absolutePathHTMl);
+// });
 
 // json response to the get method on the /json route
-app.get("/json", (req, res) => {
-  let message = "Hello json";
+// app.get("/json", (req, res) => {
+//   let message = "Hello json";
 
-  if (process.env.MESSAGE_STYLE === "uppercase") {
-    message = message.toUpperCase();
-  }
+//   if (process.env.MESSAGE_STYLE === "uppercase") {
+//     message = message.toUpperCase();
+//   }
 
-  res.json({ message });
-});
+//   res.json({ message });
+// });
 //send string response example
 // app.get("/", (req, res) => {
 //   res.send("Hello Express");
