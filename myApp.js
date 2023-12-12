210d3bb84c80171f62edfdf4d1e415bab44703c3
FairyPenguin <autorun81@gmail.com>
2023-12-12 05:54:51 +0200

    Express sendFiles method and use the __dirname global variable

--- myApp.js
+++ myApp.js
@@ -1,9 +1,15 @@
 let express = require("express");
 let app = express();
 
+const absolutePath = __dirname + "/views/index.html";
+
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  res.sendFile(absolutePath);
 });
+
+// app.get("/", (req, res) => {
+//   res.send("Hello Express");
+// });
 
 console.log("Hello World");
 
