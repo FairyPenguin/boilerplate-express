60cc73665048f922ab08cbf92a0bcb37d92b3cc7
FairyPenguin <autorun81@gmail.com>
2023-12-12 08:30:55 +0200

    Fix the wrong path to the style.css file in the index.html

--- myApp.js
+++ myApp.js
@@ -1,12 +1,19 @@
 let express = require("express");
 let app = express();
 
-const absolutePath = __dirname + "/views/index.html";
+//set the absolut path using the __dirname global variable
+const absolutePathHTMl = __dirname + "/views/index.html";
 
+const absolutePathCSS = __dirname + "/public";
+
+app.use(express.static(absolutePathCSS));
+
+// send file response example
 app.get("/", (req, res) => {
-  res.sendFile(absolutePath);
+  res.sendFile(absolutePathHTMl);
 });
 
+//send string response example
 // app.get("/", (req, res) => {
 //   res.send("Hello Express");
 // });
