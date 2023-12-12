f9d8b0b9d95ce3dfca821e20745ae3c0ac96068e
FairyPenguin <autorun81@gmail.com>
2023-12-12 15:05:30 +0200

    .env file

--- myApp.js
+++ myApp.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
 
 // json response to the get method on the /json route
 app.get("/json", (req, res) => {
-  if (process.env.MESSAGE_STYLE === uppercase) {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
     res.json({ message: "HELLO JSON" });
   } else {
     res.json({ message: "Hello json" });
